8117b2395a6bb36be57477ff5cb70fd340bfae10
Dandav2923 <daniele.emili23@gmail.com>
2021-11-19 15:52:17 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -2,10 +2,23 @@
 
 let chilometri = prompt ('Quanti Km devi fare?');
 // console.log(chilometri);
-document.getElementById ('km').innerHTML = chilometri;
+let kmDaPercorrere = 
+`
+<span>Il tuo viaggio &egrave; di </span>${chilometri} <span>Km</span>
+`;
+
+document.getElementById ('km').innerHTML = kmDaPercorrere;
+
+
 let eta = prompt ('Digita la tua età');
 // console.log(eta);
-document.getElementById('eta').innerHTML = eta;
+let etaUtente = 
+`
+<span>La tua et&agrave; &egrave; di </span>${eta} <span>anni</span>
+`;
+
+document.getElementById('eta').innerHTML = etaUtente;
+
 let prezzoAlChilometro = 0.21;
 // console.log(prezzoAlChilometro);
 let prezzo = chilometri * prezzoAlChilometro;
@@ -16,14 +29,21 @@ const minorenni = 17;
 if (eta > over65) {
     prezzo = prezzo - ((prezzo * 40)/ 100);
     // console.log(prezzo);
-    document.getElementById('prezzokm').innerHTML = prezzo;
+    document.getElementById('prezzokm').innerHTML = 
+    `
+    <span>Il prezzo del biglietto &egrave; di ${prezzo} &euro;
+    `;
 }
 else if (eta <= minorenni) {
     prezzo = prezzo - ((prezzo * 20)/100);
     // console.log(prezzo);
-    document.getElementById('prezzokm').innerHTML = prezzo;
+    document.getElementById('prezzokm').innerHTML = `
+    <span>Il prezzo del biglietto &egrave; di ${prezzo} &euro;
+    `;
 }
 else {
     // console.log(prezzo);
-    document.getElementById('prezzokm').innerHTML = prezzo;
+    document.getElementById('prezzokm').innerHTML = `
+    <span>Il prezzo del biglietto &egrave; di ${prezzo} &euro;
+    `;
 }
