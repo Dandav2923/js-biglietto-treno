f726ba4cf10005d08cf4644df149ff9b837f72a4
Dandav2923 <daniele.emili23@gmail.com>
2021-11-19 15:56:23 +0100

    insierimento commenti

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // alert ('ciao');
 
+// dichiarazione delle variabili let js
+
 let chilometri = prompt ('Quanti Km devi fare?');
 // console.log(chilometri);
 let kmDaPercorrere = 
 `
 <span>Il tuo viaggio &egrave; di </span>${chilometri} <span>Km</span>
 `;
-
 document.getElementById ('km').innerHTML = kmDaPercorrere;
 
 
@@ -16,15 +17,19 @@ let etaUtente =
 `
 <span>La tua et&agrave; &egrave; di </span>${eta} <span>anni</span>
 `;
-
 document.getElementById('eta').innerHTML = etaUtente;
+
 
 let prezzoAlChilometro = 0.21;
 // console.log(prezzoAlChilometro);
 let prezzo = chilometri * prezzoAlChilometro;
 // console.log(prezzo);
+
+// dichiarazione delle variabili const js
 const over65 = 65;
 const minorenni = 17;
+
+// elenco delle condizioni di scondo 
 
 if (eta > over65) {
     prezzo = prezzo - ((prezzo * 40)/ 100);
